c78690924e46e4f33acb152a439ffc56e7b15477
ypyao77 <30677472@qq.com>
2017-09-17 17:30:49 +0800

    spring-boot-mvc demo

---
+++ springio/webmvc/src/main/java/org/demo/web/mvc/DescribeController.java
@@ -0,0 +1,17 @@
+package org.demo.web.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DescribeController {
+
+    @RequestMapping("/describe")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "describe";
+    }
+
+}
