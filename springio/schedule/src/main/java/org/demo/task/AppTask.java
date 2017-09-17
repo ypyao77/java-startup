ef2133564f9a10c70b7681f6c97908b033b09b04
ypyao77 <30677472@qq.com>
2017-09-17 17:29:54 +0800

    spring-boot-mvc upload file

---
+++ springio/schedule/src/main/java/org/demo/task/AppTask.java
@@ -0,0 +1,13 @@
+package org.demo.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class AppTask {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppTask.class);
+    }
+}
