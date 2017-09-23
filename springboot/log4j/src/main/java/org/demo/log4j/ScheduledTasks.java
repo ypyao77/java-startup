d6bf5a694cb1e2c8c80347acc870c01487211fc8
yunping.yao <yunping.yao@feiniu.com>
2017-09-23 08:49:10 +0800

    log4j async2

--- springboot/log4j/src/main/java/org/demo/log4j/ScheduledTasks.java
+++ springboot/log4j/src/main/java/org/demo/log4j/ScheduledTasks.java
@@ -1,20 +1,21 @@
-package org.demo.task;
+package org.demo.log4j;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
-    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+    protected static final Logger logger = Logger.getLogger(ScheduledTasks.class);
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 3000)
     public void reportCurrentTime() {
-        log.info("The time is now 现在系统时间 {}", dateFormat.format(new Date()));
+        logger.info("The time is now {}" + dateFormat.format(new Date()));
     }
 }
