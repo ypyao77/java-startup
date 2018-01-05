53a0f7de89a6b013ef73f5c3b21f52978646f7c8
yunping.yao <yunping.yao@feiniu.com>
2018-01-05 11:15:09 +0800

    spring-cloud-netflix spring-cloud-zookeeper

--- spring-cloud-zookeeper/consume/src/main/java/org/demo/DemoService.java
+++ spring-cloud-zookeeper/consume/src/main/java/org/demo/DemoService.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(value = "demo-service", path = "/api/demo")
-//@RequestMapping(value = "/api/demo")
 public interface DemoService {
     @GetMapping(value = "/instance")
     String instance();
--- spring-cloud-zookeeper/produce/src/main/java/org/demo/DemoController.java
+++ spring-cloud-zookeeper/produce/src/main/java/org/demo/DemoController.java
@@ -2,6 +2,7 @@ package org.demo;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,21 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/demo")
 public class DemoController {
-
     private static final UUID INSTANCE_UUID = UUID.randomUUID();
-    private final DemoService remoteService;
-
-    public DemoController(final DemoService remoteService) {
-        this.remoteService = remoteService;
-    }
-
-    @GetMapping(value = "/remote-instance")
-    public Object remoteInstance() {
-        return this.remoteService.instance();
-    }
+    @Value("${source.name}")
+    private String sourceName;
 
     @GetMapping(value = "/instance")
     public Object instance() {
-        return INSTANCE_UUID.toString();
+        return sourceName + ": "  + INSTANCE_UUID.toString();
     }
 }
