0f8efaf2ec7d46cd5dc0ee8153f167c21ec1dca8
yunping.yao <yunping.yao@feiniu.com>
2018-01-05 19:17:58 +0800

    eurekaservice

--- spring-cloud-netflix/eurekaservice/src/main/java/org/demo/eurekaservice/EurekaServiceApplication.java
+++ spring-cloud-netflix/eurekaservice/src/main/java/org/demo/eurekaservice/EurekaServiceApplication.java
@@ -2,6 +2,7 @@ package org.demo.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
--- spring-cloud-netflix/produce/src/main/java/org/demo/hello/produce/controller/HelloController.java
+++ spring-cloud-netflix/produce/src/main/java/org/demo/hello/produce/controller/HelloController.java
@@ -2,23 +2,29 @@ package org.demo.hello.produce.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
     private final Logger logger = Logger.getLogger(getClass());
+
+    @Value("${orig.source.name")
+    private String origSourceName;
 
     @Autowired
     private DiscoveryClient client;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        ServiceInstance instance = client.getLocalServiceInstance();
-        logger.info("/hello, host:" + instance.getHost() + ", servgice_id" + instance.getServiceId());
-        return "Hello world";
+        List<String> services = client.getServices();
+        logger.info("/hello, services:" + services.toString());
+        return "[" + origSourceName + "] Hello world";
     }
 }
