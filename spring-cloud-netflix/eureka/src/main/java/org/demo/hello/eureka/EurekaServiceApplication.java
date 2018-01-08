89a549ecab387cd8a451b91a18a6df1776add522
yunping.yao <yunping.yao@feiniu.com>
2018-01-08 10:34:27 +0800

    调试spring-cloud-netflix，并支持ha

--- spring-cloud-netflix/eureka/src/main/java/org/demo/hello/eureka/EurekaServiceApplication.java
+++ spring-cloud-netflix/eureka/src/main/java/org/demo/hello/eureka/EurekaServiceApplication.java
@@ -1,8 +1,7 @@
-package org.demo.eurekaservice;
+package org.demo.hello.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
--- spring-cloud-netflix/produce/src/main/java/org/demo/hello/produce/controller/HelloController.java
+++ spring-cloud-netflix/produce/src/main/java/org/demo/hello/produce/controller/HelloController.java
@@ -9,13 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HelloController {
     private final Logger logger = Logger.getLogger(getClass());
 
-    @Value("${orig.source.name")
+    @Value("${orig.source.name}")
     private String origSourceName;
 
     @Autowired
@@ -25,6 +28,12 @@ public class HelloController {
     public String hello() {
         List<String> services = client.getServices();
         logger.info("/hello, services:" + services.toString());
-        return "[" + origSourceName + "] Hello world";
+        return "[" + this.getCurrTime() + "] (" + this.origSourceName + ") (" + UUID.randomUUID().toString() + ") Hello world." + services.toString();
+    }
+
+    private String getCurrTime() {
+        SimpleDateFormat formatter;
+        formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(new Date());
     }
 }
