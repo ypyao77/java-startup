8d25b9c9127985f28b6f1398b6a44a6b286d6dc8
yunping.yao <yunping.yao@feiniu.com>
2018-01-23 16:48:12 +0800

    spring-cloud-consul/pom.xml

---
+++ spring-cloud-consul/produce/src/main/java/org/demo/hello/produce/Application.java
@@ -0,0 +1,24 @@
+package org.demo.hello.produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@RestController
+public class Application {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
